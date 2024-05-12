27993e8512c7b21a8914754b13f2686f61d2c8ac
Marcus Reis <mvsantosreis@gmail.com>
2024-05-13 00:12:00 +0100

    showFullVideo option

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,13 @@ import { storeToRefs } from 'pinia'
 const useMovie = useMovieStore()
 const { movie, showFullVideo } = storeToRefs(useMovie)
 
+let video = ref(null)
+
 onMounted(() => {
   setTimeout(() => (movie.value = movies[0][0]), 100)
 })
 </script>
+
 <template>
   <div class="fixed w-full h-screen bg-black">
     <div
@@ -51,11 +54,13 @@ onMounted(() => {
         </div>
       </div>
     </div>
+
     <div v-if="!showFullVideo">
       <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
         <div
           class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black"
         />
+
         <MovieDetails v-if="movie" :movie="movie" />
 
         <video
@@ -74,6 +79,26 @@ onMounted(() => {
           <VideoCarousel class="pb-32" category="Featured Movies" :movies="movies[2]" />
         </div>
       </div>
+
+      <div
+        class="absolute z-20 h-[70%] left-[120px] w-[100%] right-0 bottom-0 bg-gradient-to-t from-black via-black"
+      />
+    </div>
+
+    <div v-if="showFullVideo">
+      <div
+        @click="showFullVideo = false"
+        class="absolute z-50 p-2 m-4 bg-white bg-opacity-50 rounded-full cursor-pointer"
+      >
+        <ChevronLeft fillColor="#FFFFFF" :size="40" />
+      </div>
+      <video
+        :src="'/videos/' + movie.name + '.mp4'"
+        autoplay
+        loop
+        controls
+        class="absolute z-0 w-[100vw] h-full object-fit"
+      />
     </div>
   </div>
 </template>
